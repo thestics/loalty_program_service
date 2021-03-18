279581caebb9cfd0cd2a7c1278e160b2cef1b9c9
prostoVania <kalishuk_i@ukr.net>
2021-03-18 14:27:41 +0200

    add loyalty client

--- common/clients/loyalty.py
+++ common/clients/loyalty.py
@@ -1,16 +1,29 @@
 import time
 import hashlib
+import traceback
 from datetime import datetime
 
 from flask import request
 from flask_security import current_user
-import requests
 
 
 from common.utils import serialize_dict
+from models.loyalty import Customer, ErrorLog
 
 
 class LoyaltyClient:
+
     def create_customer(self, **params):
-        pass
+        try:
+            Customer.create(**params)
+        except Exception as ex:
+            ErrorLog.create(request_data=request.data,
+                            request_ip=request.remote_addr,
+                            request_url=request.url,
+                            request_method=request.method,
+                            error=str(ex),
+                            traceback=traceback.format_exc())
+
+
+
 
