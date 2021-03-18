2d056d5814029330b3c74a4942b8b5ceda9ee732
prostoVania <kalishuk_i@ukr.net>
2021-03-18 14:26:26 +0200

    fix CustomerCreationForm

--- src/forms.py
+++ src/forms.py
@@ -1,29 +1,15 @@
 from flask import session, flash
 from flask_wtf import FlaskForm
 from wtforms import fields as wtfields
+from flask_admin.form.widgets import DatePickerWidget
+# from wtforms.ext.sqlalchemy.fields import QuerySelectField
 from wtforms.validators import InputRequired, URL, NumberRange, Optional
 
 from models.admin import user_datastore
+from models.loyalty import DiscountLevel
+from models.utils import db_wrapper
 from common.fields import PrettyJsonField
 
-
-class InvoiceCreationForm(FlaskForm):
-    secret_key = wtfields.StringField(validators=[InputRequired()])
-    partner_id = wtfields.StringField(validators=[InputRequired()])
-    partner_invoice_id = wtfields.StringField(validators=[InputRequired()])
-    payout_address = wtfields.StringField(validators=[InputRequired()])
-    callback = wtfields.StringField('Callback URL',
-                                    validators=[InputRequired(), URL()])
-    confirmations_trans = wtfields.IntegerField(
-        'Number of transit confirmations', validators=[Optional(),
-                                                       NumberRange(min=1)])
-    confirmations_payout = wtfields.IntegerField(
-        'Number of payout confirmations', validators=[Optional(),
-                                                      NumberRange(min=1)])
-    fee_level = wtfields.SelectField(choices=(('', 'None'),
-                                              ('low', 'Low'),
-                                              ('medium', 'Medium'),
-                                              ('high', 'High')))
 
 
 class CustomerCreationForm(FlaskForm):
@@ -32,7 +18,5 @@ class CustomerCreationForm(FlaskForm):
     amount_of_purchases = wtfields.IntegerField('Total amount of purchases',
                                                 validators=[InputRequired(),
                                                             NumberRange(min=0)])
-    discount_level = wtfields.IntegerField(validators=[InputRequired()])
-    birthday = wtfields.DateField(validators=[InputRequired()])
+    birthday = wtfields.DateField(validators=[InputRequired()], widget=DatePickerWidget())
     address = wtfields.StringField(validators=[Optional()])
-    family_birthdays = PrettyJsonField(validators=[Optional()])
